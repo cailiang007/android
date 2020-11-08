1c4c9f470b61d66a9f41d5d6acfc82d3ef12c8a2
cailiang <2529102378@qq.com>
2020-11-08 15:16:49 +0800

    增加功能1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -93,6 +93,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         myFirstReceiver.getDebugUnregister();
